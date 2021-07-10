7ca1fcd7bb1031a2c7157a491fe1d94668018d12
Suryansh Pandey <suryanshpsurya@gmail.com>
2021-07-10 12:39:07 +0530

    update style

--- function.js
+++ function.js
@@ -5,14 +5,17 @@ var next = document.getElementById("next");
 var back = document.getElementById("back");
 
 next.onclick = function () {
- form2.style.left = "-45rem";
- form3.style.left = "4rem";
+ form2.style.left = "-50%";
+ form3.style.left = "50%";
+ form3.style.transform = "translate(-50%)";
+
  progress.style.width = "24rem";
 }
 
 back.onclick = function () {
- form2.style.left = "5rem";
- form3.style.left = "55rem";
+ form2.style.left = "50%";
+ form3.style.left = "100%";
+ form3.style.transform = "translate(0%)";
  progress.style.width = "12rem";
 }
 
